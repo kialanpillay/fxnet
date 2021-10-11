69fa7807c8f63c3616d82f4417c0b6a91ab53b22
Kialan Pillay <kialanpillay@gmail.com>
2021-10-11 23:04:42 +0200

    Add .bifxml

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ import argparse
 
 import pandas as pd
 
+from evaluation import bayesian_network_inference, decision_network_inference
+
 
 def main():
     df = pd.read_csv("data/FXNET.csv")
     df = df[~df.isnull().any(axis=1)]
 
+    if args.test:
+        bayesian_network_inference()
+
+    decision_network_inference()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--test', action='store_true', help="Evaluation mode")
+    args = parser.parse_args()
     main()
