a9954dc57443abbd99877c6ed10b11d5b761b916
Kialan Pillay <kialanpillay@gmail.com>
2021-10-22 17:19:28 +0200

    Fix CPTs

--- networks.py
+++ networks.py
@@ -70,8 +70,12 @@ def bayesian_network():
                           'InflationRate': 'Negative'}] = [0.5, 0.3, 0.2]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Positive'}] = [0.3, 0.2, 0.5]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Negative'}] = [0.3, 0.4, 0.3]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Negative'}] = [0.2, 0.3, 0.5]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Positive'}] = [0.05, 0.05, 0.9]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.2, 0.2, 0.6]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
@@ -82,24 +86,37 @@ def bayesian_network():
                           'InflationRate': 'Negative'}] = [0.15, 0.15, 0.7]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.1, 0.1, 0.8]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
+                          'InflationRate': 'Negative'}] = [0.3, 0.4, 0.3]
+
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Negative'}] = [0.9, 0.05, 0.05]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Negative'}] = [0.6, 0.2, 0.2]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Positive',
+                          'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.5, 0.3, 0.2]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Negative'}] = [0.8, 0.2, 0.2]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Positive'}] = [0.3, 0.3, 0.6]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Negative'}] = [0.2, 0.3, 0.5]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Negative'}] = [0.2, 0.4, 0.4]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
     bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Negative'}] = [0.6, 0.2, 0.2]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Negative'}] = [0.5, 0.3, 0.2]
+    bn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Positive',
+                          'InflationRate': 'Positive'}] = [0.6, 0.2, 0.2]
 
     gum.saveBN(bn, 'output/fx_bayesian_network.bif')
     return bn
@@ -174,8 +191,12 @@ def decision_network():
                           'InflationRate': 'Negative'}] = [0.5, 0.3, 0.2]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Positive'}] = [0.3, 0.2, 0.5]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Negative'}] = [0.3, 0.4, 0.3]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Negative'}] = [0.2, 0.3, 0.5]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Positive'}] = [0.05, 0.05, 0.9]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.2, 0.2, 0.6]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
@@ -186,24 +207,37 @@ def decision_network():
                           'InflationRate': 'Negative'}] = [0.15, 0.15, 0.7]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.1, 0.1, 0.8]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Positive', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
+                          'InflationRate': 'Negative'}] = [0.3, 0.4, 0.3]
+
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Negative'}] = [0.9, 0.05, 0.05]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Negative'}] = [0.6, 0.2, 0.2]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Positive',
+                          'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.5, 0.3, 0.2]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Negative'}] = [0.8, 0.2, 0.2]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Positive'}] = [0.3, 0.3, 0.6]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Equal', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Negative'}] = [0.2, 0.3, 0.5]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Negative',
                           'InflationRate': 'Negative'}] = [0.2, 0.4, 0.4]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
     dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Positive',
                           'InflationRate': 'Negative'}] = [0.6, 0.2, 0.2]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Negative', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Positive'}] = [0.3, 0.4, 0.3]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Negative',
+                          'InflationRate': 'Negative'}] = [0.5, 0.3, 0.2]
+    dn.cpt("ClosePrice")[{'USDSentiment': 'Negative', 'InterestRate': 'Positive', 'TermsOfTrade': 'Positive',
+                          'InflationRate': 'Positive'}] = [0.6, 0.2, 0.2]
 
     U = gum.LabelizedVariable('TradeUtility', '', 1)
     dn.addUtilityNode(U)
